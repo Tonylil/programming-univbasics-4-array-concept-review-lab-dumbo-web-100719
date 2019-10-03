476c72e16eca2a30af3efd21cb442efa8df275c4
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-03 21:46:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,12 +11,12 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  array.sort()!
+  array.sort!
   return array[-1]
 end
 
 def find_min_value(array)
   # Add your solution here
-  array.sort()!
+  array.sort!
   return array[0]
 end
