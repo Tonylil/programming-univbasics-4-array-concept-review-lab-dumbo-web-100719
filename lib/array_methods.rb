6201c0d4b24ccf08a621ab074b0eef2e19e72906
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-03 21:43:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,10 @@ def find_element_index(array, value_to_find)
   length = array.length()
   length.times do |index| 
     if array[index] == value_to_find
-      
-  
+      return index
+    end
+  end
+  return nil
 end
 
 def find_max_value(array)
